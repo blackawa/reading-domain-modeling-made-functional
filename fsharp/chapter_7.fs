99210fa25df167856fda49a7e44004d6c55e48ed
blackawa <3104kurokawa@gmail.com>
2022-05-19 16:02:52 +0900

    reading chapter 7

--- fsharp/chapter_7.fs
+++ fsharp/chapter_7.fs
@@ -7,3 +7,15 @@ type Command<'data> = {
 
 type PlaceOrder = Command<UnvalidatedOrder>
 
+// 状態ごとに異なるドメインモデルを書く
+type ValidatedOrder = {
+    OrderId: OrderId
+    CustomerInfo: CustomerInfo
+    // etc...
+}
+
+// 状態の集合は直和型で表現できる
+type Order = 
+    | Unvalidated of UnvalidatedOrder
+    | Validated of ValidatedOrder
+    | Priced of PricedOrder
